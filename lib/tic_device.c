42e11c7fb01017e0e67ff94872418dd1ed389a7a
David (Pololu) <dev-david@pololu.com>
2017-07-11 17:29:59 -0700

    Changed tic_device_get_name to return a more official name.

--- lib/tic_device.c
+++ lib/tic_device.c
@@ -217,7 +217,7 @@ void tic_device_free(tic_device * device)
 const char * tic_device_get_name(const tic_device * device)
 {
   if (device == NULL) { return ""; }
-  return "Tic USB Stepper Motor Controller T825";
+  return "Tic T825 Stepper Motor Controller";
 }
 
 const char * tic_device_get_short_name(const tic_device * device)
