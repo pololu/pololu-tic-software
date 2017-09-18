99dbc4e39a6d7691b0a12ff056fb191b944f6c62
David Grayson <david@MACMINI0J111.pololu.internal>
2017-09-18 16:20:50 -0700

    Added '#include <array>' in two places to fix a GUI compilation error on macOS.

--- gui/qt/InputWizard.h
+++ gui/qt/InputWizard.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include <QWizard>
 
 class InputWizard;
--- gui/qt/main_window.h
+++ gui/qt/main_window.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <array>
+
 #include "tic.hpp"
 
 #include "bootloader_window.h"
