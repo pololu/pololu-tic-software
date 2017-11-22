56c4cedf3d7d2b9fc4205a54d595f5886b2379b1
David (Pololu) <dev-david@pololu.com>
2017-11-22 13:55:22 -0800

    Made Qt's Fusion style be the default style on non-Windows systems.
    You can override this on the command line with e.g. '-style macintosh'

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,4 +1,5 @@
 #include <QApplication>
+#include <QStyleFactory>
 #include "main_controller.h"
 #include "main_window.h"
 
@@ -8,6 +9,13 @@ int main(int argc, char ** argv)
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
+
+  // On non-Windows systems, use Qt's fusion style instead of a
+  // native style.
+#ifndef _WIN32
+  QApplication::setStyle(QStyleFactory::create("fusion"));
+#endif
+
   QApplication app(argc, argv);
   main_controller controller;
   main_window window;
