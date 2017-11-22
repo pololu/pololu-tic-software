bffac8f91491107ac733961ba45b780338d3638e
David (Pololu) <dev-david@pololu.com>
2017-11-22 13:46:41 -0800

    Removed an old TODO from gui/main.cpp.

--- gui/main.cpp
+++ gui/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, char ** argv)
 {
   // AA_EnableHighDpiScaling was added in Qt 5.6.
 #if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
-  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // todo: check if/when this actually helps
+  QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
 #endif
   QApplication app(argc, argv);
   main_controller controller;
