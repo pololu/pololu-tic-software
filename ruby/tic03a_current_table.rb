18547bd2d7bfb5e254f836d3f03b94d86b29e6ef
David (Pololu) <dev-david@pololu.com>
2018-03-05 15:15:01 -0800

    lib: Make the current-related functions return the right results for the tic03a.  The GUI is acting up now and tending to round down to a lower value even if we enter a value that is exactly right.

---
+++ ruby/tic03a_current_table.rb
@@ -0,0 +1,21 @@
+def compute_ilim(dac_level)
+  v_iset = 0.9
+  v_dac_top = 4.096
+  r_top = 137000.0
+  r_bot = 41200.0
+
+  r_dac_top = (32 - dac_level) * 5000
+  r_dac_bot = dac_level * 5000
+
+  v_dacout = v_dac_top * r_dac_bot / (r_dac_bot + r_dac_top)
+  r_dacout = 1 / (1 / r_dac_top.to_f + 1 / r_dac_bot.to_f)
+
+  iset_current_sourced =
+    (v_iset - v_dacout) / (r_top + r_dacout) + v_iset / r_bot
+
+  iset_current_sourced * 86666
+end
+
+31.downto(0) do |dac_level|
+  puts "  %d," % [(compute_ilim(dac_level) * 1000).round]
+end
