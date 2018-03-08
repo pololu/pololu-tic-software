5afb74e1869679b1ebd14dd7126d3775cc332c41
David (Pololu) <dev-david@pololu.com>
2018-03-08 11:06:37 -0800

    tic03a_current_table.rb: Don't divide by zero even though it ended up working in the end.

--- ruby/tic03a_current_table.rb
+++ ruby/tic03a_current_table.rb
@@ -1,3 +1,5 @@
+# TODO: account for latest changes to current limiting hardware
+
 def compute_ilim(dac_level)
   v_iset = 0.9
   v_dac_top = 4.096
@@ -8,7 +10,11 @@ def compute_ilim(dac_level)
   r_dac_bot = dac_level * 5000
 
   v_dacout = v_dac_top * r_dac_bot / (r_dac_bot + r_dac_top)
-  r_dacout = 1 / (1 / r_dac_top.to_f + 1 / r_dac_bot.to_f)
+  if dac_level == 0
+    r_dacout = 0
+  else
+    r_dacout = 1 / (1 / r_dac_top.to_f + 1 / r_dac_bot.to_f)
+  end
 
   iset_current_sourced =
     (v_iset - v_dacout) / (r_top + r_dacout) + v_iset / r_bot
@@ -19,3 +25,4 @@ end
 31.downto(0) do |dac_level|
   puts "  %d," % [(compute_ilim(dac_level) * 1000).round]
 end
+
