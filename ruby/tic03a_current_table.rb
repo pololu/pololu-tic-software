dca8e9526f92dbaa43e1582b111b1662d97c5d39
David (Pololu) <dev-david@pololu.com>
2018-03-12 20:03:03 -0700

    Updated tic03a current limit table.

--- ruby/tic03a_current_table.rb
+++ ruby/tic03a_current_table.rb
@@ -1,7 +1,7 @@
 def compute_ilim(dac_level)
   v_iset = 0.9
   v_dac_top = 4.096
-  r_top = 115000.0
+  r_top = 107000.0
   r_bot = 33000.0
 
   r_dac_top = (32 - dac_level) * 5000
@@ -16,6 +16,10 @@ def compute_ilim(dac_level)
 
   iset_current_sourced =
     (v_iset - v_dacout) / (r_top + r_dacout) + v_iset / r_bot
+
+  if iset_current_sourced < 0
+    iset_current_sourced = 0
+  end
 
   iset_current_sourced * 86666
 end
