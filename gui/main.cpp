0af1926a316d40f089c634ea2e0d3d9195e54054
David (Pololu) <dev-david@pololu.com>
2018-03-14 12:56:39 -0700

    gui: Add a call to SetProcessDPIAware on Windows.

--- gui/main.cpp
+++ gui/main.cpp
@@ -1,13 +1,24 @@
-#include <QApplication>
-#include <QStyleFactory>
 #include "main_controller.h"
 #include "main_window.h"
+
+#include <QApplication>
+#include <QStyleFactory>
+
+#ifdef _WIN32
+#include <windows.h>
+#endif
 
 int main(int argc, char ** argv)
 {
   // AA_EnableHighDpiScaling was added in Qt 5.6.
-#if (QT_VERSION >= QT_VERSION_CHECK(5, 6, 0))
+#if QT_VERSION >= QT_VERSION_CHECK(5, 6, 0)
   QApplication::setAttribute(Qt::AA_EnableHighDpiScaling);
+#endif
+
+  // Fix issues caused by dragging an application between two monitors with
+  // different DPIs on Windows.
+#ifdef _WIN32
+  SetProcessDPIAware();
 #endif
 
   // On non-Windows systems, use Qt's fusion style instead of a
