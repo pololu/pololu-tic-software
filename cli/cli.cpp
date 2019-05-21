33b16f486d97b39ecd591f4269f2a205fdcfd251
David (Pololu) <dev-david@pololu.com>
2019-05-21 11:41:41 -0700

    Made more progress on the redesigned API for working with current limits, but I now think this is going in the wrong direction and is not worth it.

--- cli/cli.cpp
+++ cli/cli.cpp
@@ -665,7 +665,14 @@ static void set_current_limit_after_warning(device_selector & selector, uint32_t
   tic::handle handle = ::handle(selector);
   uint8_t product = handle.get_device().get_product();
 
-  uint32_t max_current = tic_get_max_allowed_current(product);
+  // Note: We could just call handle.set_current_limit(current_limit), but
+  // we want to display a nice warning if the current limit is too high so
+  // we do some extra work.
+
+  tic::settings settings =
+    handle.get_ram_settings_needed_to_convert_current_limits();
+
+  uint32_t max_current = settings.get_max_allowed_current_v2();
   if (current_limit > max_current)
   {
     current_limit = max_current;
@@ -674,7 +681,8 @@ static void set_current_limit_after_warning(device_selector & selector, uint32_t
       << "so it will be lowered to " << current_limit << " mA." << std::endl;
   }
 
-  handle.set_current_limit(current_limit);
+  uint8_t code = settings.current_limit_encode(current_limit);
+  handle.set_current_limit_core(code);
 }
 
 static void get_status(device_selector & selector, bool full_output)
--- lib/tic_current_limit.c
+++ lib/tic_current_limit.c
@@ -62,22 +62,30 @@ static const uint8_t tic01a_recommended_codes[64] =
   96,  100, 104, 108, 112, 116, 120, 124,
 };
 
-uint32_t tic_get_max_allowed_current(uint8_t product)
+static const uint8_t tic06a_recommended_codes[128] =
 {
-  switch (product)
-  {
-  case TIC_PRODUCT_T500:
-    return TIC_MAX_ALLOWED_CURRENT_T500;
-  case TIC_PRODUCT_T834:
-    return TIC_MAX_ALLOWED_CURRENT_T834;
-  case TIC_PRODUCT_T825:
-  case TIC_PRODUCT_N825:
-    return TIC_MAX_ALLOWED_CURRENT_T825;
-  case TIC_PRODUCT_T249:
-    return TIC_MAX_ALLOWED_CURRENT_T249;
-  default:
-    return 0;
-  }
+  0,   1,   2,   3,   4,   5,   6,   7,
+  8,   9,   10,  11,  12,  13,  14,  15,
+  16,  17,  18,  19,  20,  21,  22,  23,
+  24,  25,  26,  27,  28,  29,  30,  31,
+  32,  33,  34,  35,  36,  37,  38,  39,
+  40,  41,  42,  43,  44,  45,  46,  47,
+  48,  49,  50,  51,  52,  53,  54,  55,
+  56,  57,  58,  59,  60,  61,  62,  63,
+  64,  65,  66,  67,  68,  69,  70,  71,
+  72,  73,  74,  75,  76,  77,  78,  79,
+  80,  81,  82,  83,  84,  85,  86,  87,
+  88,  89,  90,  91,  92,  93,  94,  95,
+  96,  97,  98,  99,  100, 101, 102, 103,
+  104, 105, 106, 107, 108, 109, 110, 111,
+  112, 113, 114, 115, 116, 117, 118, 119,
+  120, 121, 122, 123, 124, 125, 126, 127,
+};
+
+tic_error * tic_get_settings_needed_to_convert_current_limits(
+  tic_handle * handle, tic_settings ** settings)
+{
+  // TODO
 }
 
 const uint8_t * tic_get_recommended_current_limit_codes(
@@ -113,6 +121,11 @@ const uint8_t * tic_get_recommended_current_limit_codes(
     count = sizeof(tic01a_recommended_codes);
     break;
 
+  case TIC_PRODUCT_TIC06A:
+    table = tic06a_recommended_codes;
+    count = sizeof(tic06a_recommended_codes);
+    break;
+
   default:
     table = NULL;
     count = 0;
@@ -123,6 +136,28 @@ const uint8_t * tic_get_recommended_current_limit_codes(
   return table;
 }
 
+// Deprecated: Without access to a tic_settings object, this function cannot
+// to do the right thing for tic06a.
+uint32_t tic_get_max_allowed_current(uint8_t product)
+{
+  switch (product)
+  {
+  case TIC_PRODUCT_T500:
+    return TIC_MAX_ALLOWED_CURRENT_T500;
+  case TIC_PRODUCT_T834:
+    return TIC_MAX_ALLOWED_CURRENT_T834;
+  case TIC_PRODUCT_T825:
+  case TIC_PRODUCT_N825:
+    return TIC_MAX_ALLOWED_CURRENT_T825;
+  case TIC_PRODUCT_T249:
+    return TIC_MAX_ALLOWED_CURRENT_T249;
+  default:
+    return 0;
+  }
+}
+
+// Deprecated: Without access to a tic_settings object, this function cannot
+// to do the right thing for tic06a.
 uint32_t tic_current_limit_code_to_ma(uint8_t product, uint8_t code)
 {
   if (product == TIC_PRODUCT_T500)
@@ -133,7 +168,10 @@ uint32_t tic_current_limit_code_to_ma(uint8_t product, uint8_t code)
     }
     return tic03a_current_table[code];
   }
-  else
+  else if (product == TIC_PRODUCT_T825 ||
+    product == TIC_PRODUCT_N825 ||
+    product == TIC_PRODUCT_T834 ||
+    product == TIC_PRODUCT_T249)
   {
     uint8_t units = TIC_CURRENT_LIMIT_UNITS_MA;
     if (product == TIC_PRODUCT_T249)
@@ -149,28 +187,47 @@ uint32_t tic_current_limit_code_to_ma(uint8_t product, uint8_t code)
 
     return code * units;
   }
+  else
+  {
+    // Other products are not supported by this deprecated function.
+    return 0;
+  }
 }
 
+// Deprecated: Without access to a tic_settings object, this function cannot
+// to do the right thing for tic06a.
 uint8_t tic_current_limit_ma_to_code(uint8_t product, uint32_t ma)
 {
-  size_t count;
-  const uint8_t * table = tic_get_recommended_current_limit_codes(
-    product, &count);
-
-  // Assumption: The table is in ascending order, so we want to return the last
-  // one that is less than or equal to the desired current.
-  // Assumption: 0 is a valid code and a good default to use.
-  uint8_t code = 0;
-  for (size_t i = 0; i < count; i++)
+  if (product == TIC_PRODUCT_T500 ||
+    product == TIC_PRODUCT_T825 ||
+    product == TIC_PRODUCT_N825 ||
+    product == TIC_PRODUCT_T834 ||
+    product == TIC_PRODUCT_T249)
   {
-    if (tic_current_limit_code_to_ma(product, table[i]) <= ma)
+    size_t count;
+    const uint8_t * table = tic_get_recommended_current_limit_codes(
+      product, &count);
+
+    // Assumption: The table is in ascending order, so we want to return the last
+    // one that is less than or equal to the desired current.
+    // Assumption: 0 is a valid code and a good default to use.
+    uint8_t code = 0;
+    for (size_t i = 0; i < count; i++)
     {
-      code = table[i];
+      if (tic_current_limit_code_to_ma(product, table[i]) <= ma)
+      {
+        code = table[i];
+      }
+      else
+      {
+        break;
+      }
     }
-    else
-    {
-      break;
-    }
+    return code;
   }
-  return code;
+  else
+  {
+    // Other products are not supported by this deprecated function.
+    return 0;
+  }
 }
