846935119e65eb800b7466bc3d82156a4dc875ef
David (Pololu) <dev-david@pololu.com>
2019-05-21 16:05:41 -0700

    tic06a: Set the DRV8711 MODE bitfield correctly.

--- lib/tic_set_settings.c
+++ lib/tic_set_settings.c
@@ -267,8 +267,8 @@ static void tic_write_settings_to_buffer(const tic_settings * settings, uint8_t 
   buf[TIC_SETTING_CURRENT_LIMIT_DURING_ERROR] =
     tic_settings_get_current_limit_code_during_error(settings);
 
-  buf[TIC_SETTING_STEP_MODE] =
-    tic_settings_get_step_mode(settings);
+  uint8_t step_mode = tic_settings_get_step_mode(settings);
+  buf[TIC_SETTING_STEP_MODE] = step_mode;
 
   buf[TIC_SETTING_DECAY_MODE] = tic_settings_get_decay_mode(settings);
 
@@ -348,24 +348,38 @@ static void tic_write_settings_to_buffer(const tic_settings * settings, uint8_t 
 
     uint8_t * p = buf + TIC_SETTING_DRV8711_REGISTERS;
 
-    // CTRL register: Defaults except MODE, which is full-step.
-    p[0] = 0b00000000;
+    // Setting TORQUE accurately here typically saves the firmware from doing
+    // some work, since it synchronizes DRV8711 registers before setting the
+    // current for the first time.
+
+    // Setting MODE accurately here only matters if people want to read the
+    // DRV8711 CTRL register using a "Get settings" command.
+    uint8_t step_mode_code = step_mode;
+    if (step_mode_code >= TIC_STEP_MODE_MICROSTEP2_100P) { step_mode_code--; }
+    step_mode_code &= 0xF;
+
+    // Setting DECAY accurately here only matters if people want to read the
+    // DRV8711 DECAY register using a "Get settings" command.
+    // TODO: set DECAY
+
+    // CTRL register: Defaults except for MODE (step_mode).
+    p[0] = step_mode_code << 3;
     p[1] = 0b1100;
     // TORQUE register: Default current limit, default SMPLTH.
     p[2] = current_limit_code;
     p[3] = 0b0001;
     // OFF register: defaults.
     p[4] = 0x30;
-    p[5] = 0x0;
+    p[5] = 0;
     // BLANK register: defaults.
     p[6] = 0x80;
-    p[7] = 0x0;
+    p[7] = 0;
     // DECAY register: defaults.
     p[8] = 0x10;
     p[9] = 0b0001;
     // STALL register: defaults, except SDTHR is 0 to disable stall detection.
-    p[10] = 0x00;
-    p[11] = 0b0000;
+    p[10] = 0;
+    p[11] = 0;
     // DRIVE register: defaults.
     p[12] = 0b01011001;
     p[13] = 0b1010;
